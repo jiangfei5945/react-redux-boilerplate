3def223953e18d80c71f422313b1f59d076babd2
jiangfei <jiangfei@letv.com>
2017-02-20 17:27:37 +0800

    optimze index stream

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import Express from 'express';
 import path from 'path';
 import logger from 'morgan';
 import proxy from 'express-http-proxy';
-import fs from 'fs';
 import url from 'url';
 
 function createConsole() {
     const endpoint = 'http://localhost:8081';
     const app = new Express();
+    const indexPath = path.resolve(__dirname + '/../index.html');
+    
     app.use(logger('dev'));
     app.use('/asset', Express.static(path.resolve(__dirname, '../asset')));
     app.use('/p', proxy(endpoint, {
@@ -17,14 +18,7 @@ function createConsole() {
     }));
 
     app.get('/index', (req, res) => {
-        const html = fs
-            .readFileSync(`${__dirname}/../index.html`)
-            .toString()
-            .replace('<!-- JS_MODULE -->', `</script><script src="/dist/main.js"></script>`)
-            .replace('<!-- CSS_MODULE -->', `<link href="/dist/style.css" rel="stylesheet">`);
-
-        res.set('Content-Type', 'text/html');
-        res.send(html);
+        res.sendFile(indexPath);
     });
 
     return app;
