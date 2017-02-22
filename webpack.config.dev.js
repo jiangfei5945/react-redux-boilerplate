13034bd0986e6a207c05cb84f3717ec5f893379a
jiangfei <jiangfei@letv.com>
2017-02-22 11:41:59 +0800

    add eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,7 @@ module.exports = {
 
   module: {
     loaders: [{
-      test: /(\.js|\.jsx)$/, loader: 'babel-loader',
+      test: /(\.js|\.jsx)$/, loader: 'babel-loader!eslint-loader',
       include: [/src/]
     }]
   },
