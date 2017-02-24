2c7e90178ec9297f0adb31c8601a737a89745c6c
jiangfei <jiangfei@letv.com>
2017-02-24 18:15:31 +0800

    tolist done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import App from './components/App';
 
+const store = createStore(todoApp);
 const rootEl = document.getElementById('root');
 
-ReactDOM.render(<App />, rootEl);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootEl);
